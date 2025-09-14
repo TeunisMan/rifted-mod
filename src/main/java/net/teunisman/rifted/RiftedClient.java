2130eccd2ea5c5dd7c92d5e4bd555be58a3d6ee5
TeunisMan <Teuniswoordurk@gmail.com>
2025-09-15 00:21:17 +0200

    Initial commit

---
+++ src/main/java/net/teunisman/rifted/RiftedClient.java
@@ -0,0 +1,10 @@
+package net.teunisman.rifted;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RiftedClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/teunisman/rifted/RiftedDataGenerator.java
@@ -0,0 +1,11 @@
+package net.teunisman.rifted;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class RiftedDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
